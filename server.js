44a9bb55ef04f724c412d4c8007bf2dab7fee0a5
ridwan <ridwan.hidayat@starinvestama.co.id>
2020-03-18 14:09:08 +0700

    update db

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const bodyParser = require('body-parser');
 const trackerRoutes = require('./routers/tracker.router');
 
 const app = express();
-const url = process.env.MONGODB_URL || 'mongodb://localhost:27017';
+const url = process.env.MONGODB_URL || 'mongodb://localhost:27017/test';
 const port = process.env.PORT || 3000;
-const dbName = 'test';
+// const dbName = 'test';
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
@@ -14,7 +14,8 @@ app.set('view engine', 'ejs');
 
 const connect = async () => {
   try {
-    const db = await mongoose.connect(`${url}/${dbName}`, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+    // const db = await mongoose.connect(`${url}/${dbName}`, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+    const db = await mongoose.connect(`${url}`, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
     console.log('database connected successfully...');
   } catch (error) {
     console.log(`Connect database failed. error: ${error}`);
